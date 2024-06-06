bf0b8913aa8cc6457452ba7cebcfdfd602e3af73
AmirRave <amir.rave@gmail.com>
2024-06-06 15:28:39 +0300

    Animation and cinemachine

--- Assets/MoonShell/MoonShell.cs
+++ Assets/MoonShell/MoonShell.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MoonShell : MonoBehaviour
 {
     public Renderer[] _renderers;
+    public MaterialPropertyBlock _propBlock;
 
     public float _noiseReveal = 0;
     public float _noiseFeather = 0;
@@ -17,23 +18,36 @@ public class MoonShell : MonoBehaviour
     
     void Awake()
     {
-        
+        _propBlock = new MaterialPropertyBlock();
+        foreach (var renderer in _renderers)
+        {
+            renderer.SetPropertyBlock(_propBlock);
+        }
     }
 
     void Update()
     {
+        var glowFactor = Mathf.Pow(2, _glowIntensity);
+        var finalColor = new Color(_glow.r * glowFactor, _glow.g * glowFactor, _glow.b * glowFactor, _glow.a);
+                
+        // _propBlock.SetFloat("_NoiseReveal", _noiseReveal);
+        // _propBlock.SetFloat("_NoiseFeather", _noiseFeather);
+        // _propBlock.SetFloat("_NoiseOffset", _noiseOffset);
+        // _propBlock.SetColor("_Glow", finalColor);
+        
         foreach (var renderer in _renderers)
         {
             if (Application.isPlaying)
             {
-                
+                renderer.material.SetFloat("_NoiseReveal", _noiseReveal);
+                renderer.material.SetFloat("_NoiseFeather", _noiseFeather);
+                renderer.material.SetFloat("_NoiseOffset", _noiseOffset);
+                renderer.material.SetColor("_Glow", finalColor);
             }
             else
             {
                 var mat = new Material(renderer.sharedMaterial);
-                var glowFactor = Mathf.Pow(2, _glowIntensity);
-                var finalColor = new Color(_glow.r * glowFactor, _glow.g * glowFactor, _glow.b * glowFactor, _glow.a);
-                
+
                 mat.SetFloat("_NoiseReveal", _noiseReveal);
                 mat.SetFloat("_NoiseFeather", _noiseFeather);
                 mat.SetFloat("_NoiseOffset", _noiseOffset);
